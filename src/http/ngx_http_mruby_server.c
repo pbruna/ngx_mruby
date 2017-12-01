8386ce0e6fe3858814c1c6d6db19035b4994440e
Patricio Bruna <pbruna@itlinux.cl>
2017-11-30 23:25:32 -0300

    Fixed ngx_sock_ntop function

--- src/http/ngx_http_mruby_server.c
+++ src/http/ngx_http_mruby_server.c
@@ -101,7 +101,13 @@ static mrb_value ngx_mrb_add_listener(mrb_state *mrb, mrb_value self)
 #endif
   }
 
+// Nginx version that renamed "u" to "sockaddr" in listen options types
+// see: https://github.com/nginx/nginx/commit/5b267a55bc27f35f38d6d04d2f6284ed4d6156c0
+#if (ngx_version < NGINX_SOCKADDR_VER)
+  (void)ngx_sock_ntop(&lsopt.u.sockaddr, lsopt.socklen, lsopt.addr, NGX_SOCKADDR_STRLEN, 1);
+#else
   (void)ngx_sock_ntop(&lsopt.sockaddr.sockaddr, lsopt.socklen, lsopt.addr, NGX_SOCKADDR_STRLEN, 1);
+#endif
   if (ngx_http_add_listen(cf, cscf, &lsopt) == NGX_OK) {
     ngx_conf_log_error(NGX_LOG_INFO, cf, 0, "add listener %V via mruby", &addr);
     return mrb_true_value();
